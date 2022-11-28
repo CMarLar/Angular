e24feb4e0d4a9c414cbf051ad1a17f6409b2301b
CMarLar <c.marina.larrosa@gmail.com>
2022-11-28 17:28:03 +0100

    Primer Commit Dia 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
-const routes: Routes = [];
+//Rutas
+const routes: Routes = 
+[
+  {path:"home", component: HomeComponent},
+  {path:"formulario", component: RegistroComponent},
+  {path: '',   redirectTo: '/home', pathMatch: 'full' }//Redirige la página directamente a home
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
